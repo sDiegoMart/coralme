7aa75b20335b13c5607f644240891f6400577ba6
Rodrigo Santibanez <glucksfall@users.noreply.github.com>
2022-11-10 20:31:53 -0800

    mini version of solvemepy, renamed cobrame v2.0 as coralme and merged synechocystisme into reconstruction main directory

--- coralme/__init__.py
+++ coralme/__init__.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+import coralme.core
+import coralme.io
+import coralme.util
+#import coralme.solve
+import coralme.minicobrapy
+import coralme.minisolvemepy
+import coralme.reconstruction
