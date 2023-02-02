c4394b025fc9ba98e05d012567faa7603e561080
jdtibochab <jdtibochab@gmail.com>
2023-02-02 12:15:30 -0800

    Error only if genbank does not have good overlap

--- coralme/builder/organism.py
+++ coralme/builder/organism.py
@@ -1339,8 +1339,6 @@ class Organism(object):
 
         if gb_overlap < 1:
             raise ValueError('Overlap of M-model genes with genbank is too low ({}%)'.format(gb_overlap))
-        if file_overlap < 1:
-            raise ValueError('Overlap of M-model genes with optional files is too low ({}%)'.format(file_overlap))
 
 
         fs = get_severity(file_overlap)
