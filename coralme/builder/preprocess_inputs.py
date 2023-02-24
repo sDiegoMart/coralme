4993fd09f1a011c926867b9705c597e7def21395
Rodrigo Santibanez <glucksfall@users.noreply.github.com>
2023-02-24 14:24:53 -0800

    Minor changes to logging messages

--- coralme/builder/preprocess_inputs.py
+++ coralme/builder/preprocess_inputs.py
@@ -183,20 +183,21 @@ def complete_organism_specific_matrix(builder, data, model, output = False):
 		if len(all_mods) != 0:
 			for mods in all_mods:
 				mods = [ x for x in mods if not x.startswith('SH') ] # Sulfur transfer in [enzyme]-S-sulfanylcysteine are enzymatic reactions
-				logging.warning('The modification \'SH\' was removed. Add MetabolicReactions to transfer sulfur from cysteine and to the many acceptors.')
+				logging.warning('The modification \'SH\' was removed. Add MetabolicReaction(s) to transfer sulfur from cysteine and to the many acceptors.')
 				mods = [ x for x in mods if not x.startswith('Oxidized') ] # metabolic modification in ferredoxin and other proteins
 				mods = [ x for x in mods if not x.startswith('palmitate') ] # metabolic modification from 2agpg160 in the lpp gene
-				mods = [ x.replace('LI', 'Li') for x in mods ]
+				mods = [ x.replace('LI', 'li') for x in mods ]
 				mods = [ x.replace('lipo', 'lipoyl') for x in mods ]
 				logging.warning('The modification \'lipo\' was renamed to \'lipoyl\'.')
 				logging.warning('Add MetabolicReactions to salvage or de novo synthesis of lipoyl moieties. See https://www.genome.jp/pathway/map00785 for more information.')
 				mods = [ x.replace('NiFeCoCN2', 'NiFe_cofactor') for x in mods ]
 				logging.warning('The modification \'NiFeCoCN2\' was renamed to \'NiFe_cofactor\'.')
-				logging.warning('Add MetabolicReactions to synthetize and transfer the NiFe_cofactor into the enzyme. See https://biocyc.org/ECOLI/NEW-IMAGE?type=PATHWAY&object=PWY-8319 for more information.')
+				logging.warning('Add MetabolicReactions to synthesize and transfer the NiFe_cofactor into the final acceptor enzyme. See https://biocyc.org/ECOLI/NEW-IMAGE?type=PATHWAY&object=PWY-8319 for more information.')
 				if len(mods) != 0:
 					if 'pan4p(1)' not in mods:
 						mod_strings.append(' AND '.join(mods))
 					else:
+						# This remove all modification from holo-ACP protein. Modifications are metabolic.
 						mod_strings.append('pan4p(1)')
 		if len(mod_strings) != 0:
 			return mod_strings
