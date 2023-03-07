77774503306d04aebd7a2eeb198bc2950d8bac58
Rodrigo Santibanez <glucksfall@users.noreply.github.com>
2023-03-07 11:42:45 -0800

    small changes to logging messages

--- coralme/builder/main.py
+++ coralme/builder/main.py
@@ -2375,10 +2375,10 @@ class METroubleshooter(object):
 		logging.warning('~ '*1 + 'Troubleshooting started...')
 		logging.warning('  '*1 + 'Checking if the ME-model can simulate growth without gapfilling reactions...')
 		if self.me_model.feasibility(keys = growth_key_and_value):
-			logging.warning('  '*5 + 'Original ME-model is feasible with a tested growth rate of {:f} 1/h'.format(list(growth_value)[0]))
+			logging.warning('  '*1 + 'Original ME-model is feasible with a tested growth rate of {:f} 1/h'.format(list(growth_value)[0]))
 			works = True
 		else:
-			logging.warning('  '*5 + 'Original ME-model is not feasible with a tested growth rate of {:f} 1/h'.format(list(growth_value)[0]))
+			logging.warning('  '*1 + 'Original ME-model is not feasible with a tested growth rate of {:f} 1/h'.format(list(growth_value)[0]))
 			works = False
 
 		# Step 1. Find topological gaps
