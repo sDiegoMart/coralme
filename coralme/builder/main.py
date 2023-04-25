eb7da4ac6534f982dec43a2051e9c159c44fa392
Rodrigo Santibanez <glucksfall@users.noreply.github.com>
2023-04-25 14:10:47 -0700

    New query method in process_data allows look for IDs to not rely on the position in the list

--- coralme/builder/main.py
+++ coralme/builder/main.py
@@ -1920,8 +1920,8 @@ class MEReconstruction(MEBuilder):
 		if 'CPLX_dummy' in me.global_info['complex_cofactors']['fes_transfers']:
 			for fes in ['2fe2s', '4fe4s']:
 				coralme.util.building.add_complex_to_model(me, 'CPLX_dummy_mod_{:s}(1)'.format(fes), { 'protein_dummy' : 1.0, fes + '_c': 1.0})
-			list(me.complex_data)[-2].create_complex_formation()
-			list(me.complex_data)[-1].create_complex_formation()
+			me.complex_data.query('CPLX_dummy_mod_2fe2s\(1\)')[0].create_complex_formation()
+			me.complex_data.query('CPLX_dummy_mod_4fe4s\(1\)')[0].create_complex_formation()
 
 		# ### 7) Associate Complexes to Metabolic reactions and build the ME-model metabolic network
 
