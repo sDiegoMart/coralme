5531accbc29340a7477439fe26c03ba98df66138
Juan <jdtibochab@gmail.com>
2023-04-25 09:53:54 -0700

    Allow for new ribosome subreactions without manually adding them in global_info

--- coralme/builder/preprocess_inputs.py
+++ coralme/builder/preprocess_inputs.py
@@ -394,7 +394,7 @@ def complete_organism_specific_matrix(builder, data, model, output = False):
 			tags = [ str(x).split(';') for x in tags ]
 			for tag in [ x for y in tags for x in y ]:
 				if '{:s}-MONOMER'.format(tag) in lst or tag.split(':')[0] in lst or 'generic_{:s}'.format(tag) in lst:
-					return 'Ribosome_' + key
+					return 'Ribosome_' + key if 'Ribosome_' not in key else key
 
 	dct = { k:[ x.split('_mod_')[0] for x in [v['enzyme']] ] for k,v in builder.org.ribosome_subreactions.items() }
 	data['ME-model SubReaction'] = data.apply(lambda x: get_ribosome_subrxns(x, dct), axis = 1)
