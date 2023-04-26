da9b4efb0fbff0891262d75def75fe3574e25bba
Rodrigo Santibanez <glucksfall@users.noreply.github.com>
2023-04-26 13:11:34 -0700

    change in the order of lipid moficiations

--- coralme/builder/translocation.py
+++ coralme/builder/translocation.py
@@ -113,8 +113,8 @@ def add_lipoprotein_formation(model, compartment_dict, lipoprotein_precursors, l
 
 			for mod in lipid_modifications:
 				reaction_prefix = protein + '_lipid_modification_' + mod
-				add_lipoprotein_data_and_reaction(mod, 'pg160_p')
 				add_lipoprotein_data_and_reaction(mod, 'pe160_p')
+				add_lipoprotein_data_and_reaction(mod, 'pg160_p')
 		else:
 			continue
 			#print('problem with', 'protein_' + protein)
