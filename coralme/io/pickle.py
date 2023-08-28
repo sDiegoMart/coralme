714d5a7e41a66919c38021cab784116bb068d5cc
Rodrigo Santibanez <glucksfall@users.noreply.github.com>
2023-08-28 10:34:15 -0700

    Change variable names

--- coralme/io/pickle.py
+++ coralme/io/pickle.py
@@ -1,9 +1,9 @@
 import pickle
 
-def load_pickle_me_model(directory):
-    with open(directory, "rb") as f:
-        return pickle.load(f)
+def load_pickle_me_model(path):
+    with open(path, "rb") as infile:
+        return pickle.load(infile)
 
-def save_pickle_me_model(me, directory):
-    with open(directory, "wb") as f:
-        pickle.dump(me, f)
+def save_pickle_me_model(me, path):
+    with open(path, "wb") as outfile:
+        pickle.dump(me, outfile)
